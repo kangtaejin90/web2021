4d4dad13535e44dd17eb30d55e97900d29675374
kangtaejin90 <75105112+kangtaejin90@users.noreply.github.com>
2021-01-15 16:59:16 +0900

    up

---
+++ 02php/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>homepage</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header id="header">
+	<h1><a href="#">LOGO</a></h1>
+	<nav class="gnb">
+		<ul class="clearfix">
+			<li><a href="#">MENU1</a></li>
+			<li><a href="#">MENU2</a></li>
+			<li><a href="#">MENU3</a></li>
+			<li><a href="#">MENU4</a></li>
+			<li><a href="#">MENU5</a></li>
+		</ul>
+	</nav>
+</header>
